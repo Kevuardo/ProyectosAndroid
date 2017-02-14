8ed515a6ac1f3684eb262ebe6f1787eb86ff6cba
Kevin Castillo Escudero <kcastilloescudero@gmail.com>
2017-02-14 14:03:41 +0100

    Comentario

--- AgendaPersonal/app/src/main/java/com/kcastilloe/agendapersonal/MainActivity.java
+++ AgendaPersonal/app/src/main/java/com/kcastilloe/agendapersonal/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lvListaContactos = (ListView) findViewById(R.id.lvListaContactos);
+        //lvListaContactos = (ListView) findViewById(R.id.lvListaContactos);
         rellenarLista(lvListaContactos);
     }
 
