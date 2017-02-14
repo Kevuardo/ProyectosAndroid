58e62344bc37b38264b9aa8bfb60b036f70c9b00
Kevin Castillo Escudero <kcastilloescudero@gmail.com>
2017-02-14 14:04:38 +0100

    Se añade el campo id.
    Se añade id con getters y setters y un nuevo constructor al que se le
    pasa este campo como parámetro.

--- AgendaPersonal/app/src/main/java/com/kcastilloe/agendapersonal/modelo/Contacto.java
+++ AgendaPersonal/app/src/main/java/com/kcastilloe/agendapersonal/modelo/Contacto.java
@@ -4,13 +4,14 @@ import android.location.Location;
 
 public class Contacto {
 
+    private int id = 0;
     private String nombre = null;
     private String telefono = null;
     private String direccion = null;
     private String email = null;
 
 
-    /* Constructor. */
+    /* Constructor sin id (para la inserción en la BD). */
 
     public Contacto(String nombre,  String telefono, String direccion, String email) {
         this.nombre = nombre;
@@ -19,7 +20,25 @@ public class Contacto {
         this.email = email;
     }
 
+    /* Constructor con id (para la recuperación de datos en la BD y posterior muestra en el ListView). */
+
+    public Contacto(int id, String nombre, String telefono, String direccion, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.email = email;
+    }
+
     /* Getters y Setters. */
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getNombre() {
         return nombre;
