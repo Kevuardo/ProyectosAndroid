5bb1ff8f06b4cbcf4e48bef97073747781b4d242
Kevin Castillo Escudero <kcastilloescudero@gmail.com>
2017-02-15 14:31:46 +0100

    Cambios mínimos

--- AgendaPersonal/app/src/main/java/com/kcastilloe/agendapersonal/MainActivity.java
+++ AgendaPersonal/app/src/main/java/com/kcastilloe/agendapersonal/MainActivity.java
@@ -1,26 +1,39 @@
 package com.kcastilloe.agendapersonal;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.kcastilloe.agendapersonal.modelo.Contacto;
 import com.kcastilloe.agendapersonal.persistencia.GestorBBDD;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView lvListaContactos; /* La lista de contactos actual. */
-    private MenuItem botonBuscar;
-    private MenuItem botonAjustes;
+    private TextView tvCabeceraContador;
+    private ProgressBar pbProgresoCarga;
+    private MenuItem botonBuscar, botonAjustes;
     private GestorBBDD gbd;
     private ListaPersonalizada adaptadorLista;
     private ArrayList<Contacto> alContactos = new ArrayList();
@@ -31,15 +44,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tvCabeceraContador = (TextView) findViewById(R.id.tvCabeceraContador);
+        pbProgresoCarga = (ProgressBar) findViewById(R.id.pbProgresoCarga);
         lvListaContactos = (ListView) findViewById(R.id.lvListaContactos);
-        //rellenarLista(lvListaContactos);
     }
 
     /* Este método es llamado cuando la actividad pasa a primer plano, incluyendo el inicio de la app. */
     @Override
     protected void onResume(){
         super.onResume();
-
+        rellenarLista(lvListaContactos);
     }
 
     /* Cuando se cree el menú será con esta disposición. */
@@ -49,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    /* Analiza cuándo se selecciona un item del ActionBar y la acción a realizar según el que se seleccione. */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -88,7 +103,8 @@ public class MainActivity extends AppCompatActivity {
             alId.add(idContacto);
         }
 
-        adaptadorLista = new ListaPersonalizada(MainActivity.this, alNombres, alTelefonos, alId/*, idImagenContacto*/);
+        adaptadorLista = new ListaPersonalizada(MainActivity.this, R.layout.item_lista_layout, alContactos);
+
         lvListaContactos.setAdapter(adaptadorLista);
         lvListaContactos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
@@ -103,15 +119,11 @@ public class MainActivity extends AppCompatActivity {
 
     /* Sirve para abrir la actividad necesaria para crear un nuevo contacto. */
     public void crearContacto(View view){
-        boolean contactoCreado = false;
-
         Intent intentCambio = new Intent(this, NuevoContactoActivity.class);
         startActivity(intentCambio);
-        /* La actividad de añadir un contacto devuelve un booleano para evaluar
-        si el contacto se ha añadido o no. */
-
 
         /* NOTA: PREGUNTAR A ALGUIEN QUE SEPA SI SE PUEDE EVALUAR CÓMO SE CIERRA UNA ACTIVIDAD,
         * PARA SABER SI SE PUEDE EVALUAR SI EL USUARIO LA CIERRA DANDO HACIA ATRÁS O GUARDANDO CONTACTO. */
     }
+
 }
