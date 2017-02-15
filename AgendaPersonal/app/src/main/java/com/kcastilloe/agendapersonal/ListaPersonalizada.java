cb75a1fef87762b8517be0c08cd0b08dca8013d6
Kevin Castillo Escudero <kcastilloescudero@gmail.com>
2017-02-15 14:31:35 +0100

    Cambios en la clase (constructor y getView).

--- AgendaPersonal/app/src/main/java/com/kcastilloe/agendapersonal/ListaPersonalizada.java
+++ AgendaPersonal/app/src/main/java/com/kcastilloe/agendapersonal/ListaPersonalizada.java
@@ -1,6 +1,8 @@
 package com.kcastilloe.agendapersonal;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,40 +10,50 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.kcastilloe.agendapersonal.modelo.Contacto;
+
 import java.util.ArrayList;
 
-public class ListaPersonalizada extends ArrayAdapter<String> {
+public class ListaPersonalizada extends ArrayAdapter<Contacto> {
 
     private Activity contexto;
-    private ArrayList<String> alNombres = new ArrayList();
-    private ArrayList<String> alTelefonos = new ArrayList();
-    private ArrayList<Integer> alId = new ArrayList();
-    //private final Integer[] imagenContacto;
+    private ArrayList<Contacto> alContactos = new ArrayList();
     private TextView tvNombre, tvTelefono;
+    private int idVistaElemento = 0;
 
-    public ListaPersonalizada (Activity contexto, ArrayList nombresContacto, ArrayList telefonosContacto, ArrayList idsContacto /*, Integer[] imagenContacto*/) {
-        super(contexto, R.layout.item_lista_layout, nombresContacto);
-        this.contexto = contexto;
-        this.alNombres = nombresContacto;
-        this.alTelefonos = telefonosContacto;
-        this.alId = idsContacto;
-        //this.imagenContacto = imagenContacto;
+    public ListaPersonalizada(Context context, int idVistaElemento, ArrayList<Contacto> alContactos) {
+        super(context, idVistaElemento, alContactos);
+        this.idVistaElemento = idVistaElemento;
+        this.alContactos = alContactos;
     }
 
     /* Recoge la vista en la que se ha de hacer el volcado de datos sobre la lista. */
+
     @Override
-    public View getView(int position, View view, ViewGroup parent) {
-        LayoutInflater inflater = contexto.getLayoutInflater();
-        View vistaFila = inflater.inflate(R.layout.item_lista_layout, null, true);
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View vistaFila = convertView;
+        if (vistaFila == null) {
+        //Creación de la vista – general
+            LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            vistaFila = inflater.inflate(idVistaElemento, parent, false);
+        }
 
+        /*LayoutInflater inflater = contexto.getLayoutInflater();
+        View vistaFila = inflater.inflate(R.layout.item_lista_layout, null, true);*/
+
+
+
+        //Creación de la vista – TextView
         tvNombre = (TextView) vistaFila.findViewById(R.id.tvNombreItem);
+        tvNombre.setText(alContactos.get(position).getNombre());
         tvTelefono = (TextView) vistaFila.findViewById(R.id.tvTelefonoItem);
+        tvNombre.setText(alContactos.get(position).getTelefono());
 
-        /*ImageView imageView = (ImageView) vistaFila.findViewById(R.id.img);
-        tvNombre.setText("");
-
-        imageView.setImageResource(imageId[position]);*/
+        //Creación de la vista – ImageView
+        /*ImageView imagen = (ImageView) vistaFila.findViewById(R.id.imagen1);
+        Drawable drawable = getContext().getResources().getDrawable(getContext().getResources().getIdentifier("@drawable/" + alContactos.get(position).getImagen(),null, getContext().getPackageName()));
+        imagen.setImageDrawable(drawable);*/
+        //Devolución de la vista
         return vistaFila;
     }
-
 }
