2ef084179768981d5b35ed5068348f1dd14a6fff
Kevin Castillo Escudero <kcastilloescudero@gmail.com>
2017-02-24 00:30:51 +0100

    Ahora consigue llamar

--- AgendaPersonal/app/src/main/java/com/kcastilloe/agendapersonal/MainActivity.java
+++ AgendaPersonal/app/src/main/java/com/kcastilloe/agendapersonal/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private Contacto contactoAlmacenado;
     private ArrayList<Contacto> alContactos = new ArrayList();
     private Intent intentCambio;
+    private boolean listaVacia = false;
     private int idItemLista = 0; /* El id del item sobre el que se abre el menú contextual. */
 
     /* Este método es llamado cuando se crea la actividad. */
@@ -58,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         actualizarCabecera();
     }
 
-    /* Cuando se cree el menú será con esta disposición. */
+    /* Cuando se cree el menú del ActionBar será con esta disposición. */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -70,17 +71,45 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_info:
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setMessage("Creado por Kevin Castillo Escudero, 2017.\n\nContacto: kcastilloescudero@gmail.com");
-                builder.setTitle("Información de la app");
-                builder.setPositiveButton("Cerrar info", new DialogInterface.OnClickListener() {
+                AlertDialog.Builder builderInfo = new AlertDialog.Builder(this);
+                builderInfo.setMessage("Creado por Kevin Castillo Escudero, 2017.\n\nContacto: kcastilloescudero@gmail.com");
+                builderInfo.setTitle("Información de la app");
+                builderInfo.setPositiveButton("Cerrar info", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
 
                     }
                 });
-                AlertDialog dialog = builder.create();
-                dialog.show();
+                AlertDialog dialogInfo = builderInfo.create();
+                dialogInfo.show();
+                return true;
+            case R.id.action_vaciar:
+                AlertDialog.Builder builderVaciar = new AlertDialog.Builder(this);
+                builderVaciar.setMessage("¿Eliminar todos los contactos de la agenda?");
+                builderVaciar.setTitle("Vaciar agenda");
+                builderVaciar.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        try {
+                            listaVacia = gbd.eliminarTodosContactos();
+                            if (listaVacia) {
+                                Toast.makeText(MainActivity.this, "Se han borrado todos los contactos con éxito.", Toast.LENGTH_SHORT).show();
+                                rellenarLista();
+                                actualizarCabecera();
+                            }
+                        } catch (Exception e) {
+                            Toast.makeText(MainActivity.this, "Error al vaciar la lista de contactos.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                builderVaciar.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                AlertDialog dialogVaciar = builderVaciar.create();
+                dialogVaciar.show();
                 return true;
             case R.id.action_extra:
                 Toast.makeText(this, "Próximamente disponible.", Toast.LENGTH_SHORT).show();
@@ -134,6 +163,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /* Cuando se cree el menú contextual de los items del ListView será con esta disposición. */
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
@@ -141,6 +171,7 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu_contextual_listview, menu);
     }
 
+    /* Analiza cuándo se selecciona un item del menú contextual y la acción a realizar según el que se seleccione. */
     @Override
     public boolean onContextItemSelected(MenuItem item) {
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
@@ -215,6 +246,7 @@ public class MainActivity extends AppCompatActivity {
         try {
             Intent intentLlamada = new Intent(Intent.ACTION_CALL);
             intentLlamada.setData(Uri.parse(telefono));
+            intentLlamada.setData(Uri.parse("tel:" + telefono));
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
                 return;
             }
